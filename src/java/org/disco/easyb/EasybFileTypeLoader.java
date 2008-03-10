f7be121abc45b4b839ffdb953152a4497a776298
rfciii <rfciii@9bebd20b-ac44-0410-9e34-335f4663fabf>
2008-03-10 00:59:03 +0000

    Issue 29 Added an easyb file type association
    git-svn-id: http://easyb.googlecode.com/svn/trunk@120 9bebd20b-ac44-0410-9e34-335f4663fabf

---
+++ src/java/org/disco/easyb/EasybFileTypeLoader.java
@@ -0,0 +1,31 @@
+package org.disco.easyb;
+
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.components.ApplicationComponent;
+import com.intellij.openapi.fileTypes.FileTypeManager;
+import com.intellij.openapi.fileTypes.LanguageFileType;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+
+public class EasybFileTypeLoader implements ApplicationComponent {
+    public static final LanguageFileType EASYBSPEC = new EasybFileType();
+
+    public void initComponent() {
+        ApplicationManager.getApplication().runWriteAction(
+                new Runnable() {
+                    public void run() {
+                        FileTypeManager.getInstance().registerFileType(EASYBSPEC, "story");
+                    }
+                }
+        );
+    }
+
+    public void disposeComponent() {
+    }
+
+    @NonNls
+    @NotNull
+    public String getComponentName() {
+        return "easyb.filetype.loader";
+    }
+}
