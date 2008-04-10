7298235e3d53b6702853d41830853813c42fabf7
rfciii <rfciii@9bebd20b-ac44-0410-9e34-335f4663fabf>
2008-04-10 23:11:04 +0000

    Fixing problem with tree view not displaying more then one specification result
    git-svn-id: http://easyb.googlecode.com/svn/trunk@263 9bebd20b-ac44-0410-9e34-335f4663fabf

--- src/main/java/org/easyb/plugin/ui/SwingEasybView.java
+++ src/main/java/org/easyb/plugin/ui/SwingEasybView.java
@@ -7,27 +7,25 @@ import javax.swing.tree.DefaultTreeModel;
 import javax.swing.tree.TreePath;
 
 import org.easyb.plugin.ui.EasybView;
+import static org.easyb.plugin.ui.TreeUtil.expandAll;
 
 public class SwingEasybView extends JPanel implements EasybView {
     private DefaultMutableTreeNode root;
-    private DefaultTreeModel model;
-    private JTree tree;
+    JTree tree;
 
     public SwingEasybView() {
         setLayout(new BorderLayout());
 
         root = new DefaultMutableTreeNode();
-        model = new DefaultTreeModel(root);
-
         tree = new JTree(root);
         tree.setRootVisible(false);
 
-        add(tree, BorderLayout.CENTER);
+        add(new JScrollPane(tree), BorderLayout.CENTER);
     }
 
     public void addSpecResult(String message) {
         DefaultMutableTreeNode node = new DefaultMutableTreeNode(message);
-        model.insertNodeInto(node, root, root.getChildCount());
-        tree.scrollPathToVisible(new TreePath(node.getPath()));
+        ((DefaultTreeModel) tree.getModel()).insertNodeInto(node, root, root.getChildCount());
+        TreeUtil.expandAll(tree, true);
     }
 }
--- src/test/groovy/org/easyb/plugin/ui/SwingEasybViewHarness.groovy
+++ src/test/groovy/org/easyb/plugin/ui/SwingEasybViewHarness.groovy
@@ -1,15 +1,23 @@
 package org.easyb.plugin.ui
 
 import javax.swing.JFrame
+import groovy.ui.Console
 
 class SwingEasybViewHarness {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
         SwingEasybView view = new SwingEasybView()
+        frame.add(view);
+        frame.show();
+        frame.setSize(300, 200);
+        
         view.addSpecResult("Scenario A");
         view.addSpecResult("Scenario B");
         view.addSpecResult("Scenario C");
-        frame.add(view);
-        frame.show();
+
+        Console console = new Console()
+        console.setVariable('frame', frame)
+        console.setVariable('view', view)
+        console.run()
     }
 }
