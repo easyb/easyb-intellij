9aad2090c5d8c340dfa81a19dccb45397fd4f02f
rfciii <rfciii@9bebd20b-ac44-0410-9e34-335f4663fabf>
2008-04-25 03:03:05 +0000

    Issue 37 make project before running easyb plugin
    git-svn-id: http://easyb.googlecode.com/svn/trunk@314 9bebd20b-ac44-0410-9e34-335f4663fabf

--- src/main/java/org/easyb/idea/runner/EasybRunProfileState.java
+++ src/main/java/org/easyb/idea/runner/EasybRunProfileState.java
@@ -45,10 +45,10 @@ public class EasybRunProfileState extends JavaCommandLineState {
 
         JavaParameters javaParameters = new JavaParameters();
         javaParameters.setJdk(ModuleRootManager.getInstance(module).getJdk());
-        javaParameters.getClassPath().add(PathUtil.getJarPathForClass(getClass()));
         javaParameters.getClassPath().add(PathUtil.getJarPathForClass(BehaviorRunner.class));
-        javaParameters.getClassPath().add(PathUtil.getJarPathForClass(GroovyObject.class));
         addProjectClasspath(javaParameters);
+        javaParameters.getClassPath().add(PathUtil.getJarPathForClass(getClass()));
+        javaParameters.getClassPath().add(PathUtil.getJarPathForClass(GroovyObject.class));
         javaParameters.setMainClass("org.easyb.plugin.remoting.RemoteRunner");
         javaParameters.getProgramParametersList().add(Integer.toString(listener.getPort()));
         javaParameters.getProgramParametersList().add(specificationPath);
@@ -59,6 +59,10 @@ public class EasybRunProfileState extends JavaCommandLineState {
         for (VirtualFile file : getProjectClasspath()) {
             javaParameters.getClassPath().add(file);
         }
+    }
+
+    public Module[] getModulesToCompile() {
+        return new Module[]{module};
     }
 
     @SuppressWarnings("deprecation")
