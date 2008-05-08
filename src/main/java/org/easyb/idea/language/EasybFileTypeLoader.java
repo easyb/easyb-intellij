f10890cb621f82688b273471e0affd187b628b31
rfciii <rfciii@9bebd20b-ac44-0410-9e34-335f4663fabf>
2008-05-08 14:26:53 +0000

    Assigning easyb extensions to groovy by default, this enables java and groovy refactorings in easyb behaviors
    git-svn-id: http://easyb.googlecode.com/svn/trunk@333 9bebd20b-ac44-0410-9e34-335f4663fabf

--- src/main/java/org/easyb/idea/language/EasybFileTypeLoader.java
+++ src/main/java/org/easyb/idea/language/EasybFileTypeLoader.java
@@ -2,20 +2,28 @@ package org.easyb.idea.language;
 
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.ApplicationComponent;
+import com.intellij.openapi.fileTypes.FileType;
 import com.intellij.openapi.fileTypes.FileTypeManager;
-import com.intellij.openapi.fileTypes.LanguageFileType;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 
 public class EasybFileTypeLoader implements ApplicationComponent {
-    public static final LanguageFileType EASYBSPEC = new EasybFileType();
-
     public void initComponent() {
+        FileTypeManager manager = FileTypeManager.getInstance();
+
+        for (final FileType type : manager.getRegisteredFileTypes()) {
+            if (type.getDefaultExtension().equals("groovy")) {
+                addEasybAssociations(type);
+            }
+        }
+    }
+
+    private void addEasybAssociations(final FileType type) {
         ApplicationManager.getApplication().runWriteAction(
                 new Runnable() {
                     public void run() {
-                        FileTypeManager.getInstance().registerFileType(EASYBSPEC, "story");
-                        FileTypeManager.getInstance().registerFileType(EASYBSPEC, "specification");
+                        FileTypeManager.getInstance().registerFileType(type, "story");
+                        FileTypeManager.getInstance().registerFileType(type, "specification");
                     }
                 }
         );
